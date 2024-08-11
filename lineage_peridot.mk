e4243e3cb9011b95a224c15c7654a89c316fb7b2
athizz2005 <atharva15.bgmi@gmail.com>
2024-08-11 16:14:35 +0000

    peridot: Bringup @RisingTechOSS

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024 RisingOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common RisingOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from peridot device
@@ -26,8 +26,23 @@ PRODUCT_SYSTEM_DEVICE := peridot
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="peridot_global-user 14 UKQ1.240116.001 V816.0.5.0.UNPMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
+    RISING_MAINTAINER="Atharva🤝Vikas" \
+    RISING_CHIPSET="SD 8SGEN3"
 
 BUILD_FINGERPRINT := POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.5.0.UNPMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# RisingTechOSS Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# GMS
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+
+# Maintainer
+RISING_MAINTAINER=Atharva🤝Vikas
