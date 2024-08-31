48c021c3790b3b51d6a59e15cf3501d0f09ac05f
John Galt <johngaltfirstrun@gmail.com>
2024-08-31 14:03:38 -0400

    peridot: Build `libusbhost.vendor`
     * qvrservice needs this

--- device.mk
+++ device.mk
@@ -538,7 +538,8 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti
+    android.hardware.usb.gadget-service.qti \
+    libusbhost.vendor
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
