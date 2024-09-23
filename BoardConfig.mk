18d1d78229fd303c54b60ed9d7177c7cb591445e
athizz2005 <atharva15.bgmi@gmail.com>
2024-09-23 10:18:38 +0200

    peridot: add prop phoneinfo add all neccessary things for about device page

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,7 +177,9 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
+TARGET_SYSTEM_PROP += \
+     $(DEVICE_PATH)/props/system.prop \
+     $(DEVICE_PATH)/props/phoneinfo.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/props/system_ext.prop
 
 # Recovery
