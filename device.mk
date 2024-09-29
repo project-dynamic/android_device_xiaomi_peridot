99be7f52964581374ce79a3333305a884d20fed6
ramshell688 <ramonzost68@gmail.com>
2024-09-30 03:38:13 +0700

    peridot: Drop lineage fastcharge

--- device.mk
+++ device.mk
@@ -187,10 +187,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi
-
 # Fingerprint
 TARGET_HAS_UDFPS := true
 
