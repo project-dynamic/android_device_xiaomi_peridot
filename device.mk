e15a191aaa2290a456d080b01ebaabd6742fd6a0
zatvio <lordkazuya1@gmail.com>
2025-01-11 16:50:45 +0000

    peridot: Build Xiaomi Touch HIDL

--- device.mk
+++ device.mk
@@ -533,6 +533,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
