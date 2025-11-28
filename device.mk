dd68cf534347182bb8a369784dc0e317a8fe666f
Michael Bestas <mkbestas@lineageos.org>
2025-11-28 13:13:53 +0200

    peridot: overlay: Remove Dialer overlay
    Call recording is now enabled by default
    
    Change-Id: Ie454647046b9a2e4b2129e3f04352dd08073c744

--- device.mk
+++ device.mk
@@ -318,7 +318,6 @@ PRODUCT_PACKAGES += \
     ApertureOverlayPeridot \
     CarrierConfigOverlayPeridot \
     FrameworkOverlayPeridot \
-    LineageDialerOverlayPeridot \
     LineageSDKOverlayPeridot \
     LineageSettingsOverlayPeridot \
     NfcOverlayPeridot \
