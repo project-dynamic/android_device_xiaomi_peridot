982da4bc431f71230863aa6c86323145e2a04f44
romgharti <romgharti@gmail.com>
2025-12-29 07:39:41 +0000

    peridot: Add Support GameBar

--- device.mk
+++ device.mk
@@ -176,6 +176,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer3-V2-ndk.vendor \
