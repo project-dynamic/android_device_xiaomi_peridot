3064591ac967c434b991f75d75d77cc81f2a5634
athizz2005 <atharva15.bgmi@gmail.com>
2026-04-23 08:07:21 +0000

    peridot: add vendorsetup.sh add dependencies for easy cloing :)

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+git clone https://gitlab.com/sm8635-peridot/proprietary_vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
+
+git clone https://github.com/sm8635-peridot/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
+
+git clone https://github.com/sm8635-peridot/packages_apps_GameBar.git packages/apps/GameBar
+
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8635-devicetrees kernel/xiaomi/sm8635-devicetrees
+
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8635-modules kernel/xiaomi/sm8635-modules
+
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8635 kernel/xiaomi/sm8635
+
+git clone https://gitlab.com/sm8635-peridot/proprietary_vendor_xiaomi_peridot.git vendor/xiaomi/peridot
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
