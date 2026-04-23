de9ea1477700ca1474ca82de18bcb5cf2110b81d
athizz2005 <atharva15.bgmi@gmail.com>
2026-04-23 08:04:07 +0000

    peridot: nuke Viper4X we already have dolby

--- device.mk
+++ device.mk
@@ -460,8 +460,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
-# ViPER4Android FX
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
